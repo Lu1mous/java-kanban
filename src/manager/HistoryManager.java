ff7c7bab1d4fabc16ce9a15ce2784db4236640ee
Uriy Tokarev <lu1mous@yandex.ru>
2024-03-10 21:17:50 +0500

    Реализовать ТЗ 6

--- src/manager/HistoryManager.java
+++ src/manager/HistoryManager.java
@@ -2,14 +2,16 @@ package manager;
 
 import tasks.Task;
 
-import java.util.Collection;
 import java.util.List;
 
 
 public interface HistoryManager {
     void add(Task task);
+
     void remove(int id);
+
     void clear();
+
     List<Task> getHistory();
 
 }
